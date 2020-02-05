3481d11e47692e2a3213aded9fc38c1d0e9a8208
Filip Dimitrievski <f_dimitrievski@outlook.com>
2020-02-04 23:33:35 -0500

    Empty fields bug fixed and friendly msgs.

--- assignments/views.py
+++ assignments/views.py
@@ -2,28 +2,32 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import AssignmentTeacherSide, studentAssignments
 from django.contrib.auth.models import User
 from .models import studentAssignments
+from django.contrib import messages
 
 def create(request):
-    if request.method == 'POST':
-        if request.POST['title'] and request.POST['date'] and request.POST['instructions']:
-            assignment = AssignmentTeacherSide()
+    try:
+        if request.method == 'POST':
+            if request.POST['title'] and request.POST['date'] and request.POST['instructions']:
+                assignment = AssignmentTeacherSide()
 
-            assignment.title = request.POST['title']
-            assignment.dueDate = request.POST['date']
-            assignment.instructions = request.POST['instructions']
-            assignment.maxPoint = request.POST['points']
-            assignment.teacherUser = request.user
-            assignment.teacherDocument = request.FILES['teacherfile']
-            assignment.videoLink = request.POST['video']
-            assignment.save()
+                assignment.title = request.POST['title']
+                assignment.dueDate = request.POST['date']
+                assignment.instructions = request.POST['instructions']
+                assignment.maxPoint = request.POST['points']
+                assignment.teacherUser = request.user
+                assignment.teacherDocument = request.FILES['teacherfile']
+                assignment.videoLink = request.POST['video']
+                assignment.save()
 
-            return redirect('home')
+                messages.info(request, 'Assignment Created!')
+                return redirect('home')
+            else:
+                return render(request, 'assignments/create.html', {"error":"All fields are required!"})
         else:
-            return render(request, 'assignments/create.html', {"error":"All fields are required!"})
-    else:
+            return render(request, 'assignments/create.html')
+    except:
+        messages.info(request, 'Assignment Not Created!')
         return render(request, 'assignments/create.html')
-
-    return render(request, 'assignments/create.html')
 
 
 def detail(request, assignment_id):
@@ -48,25 +52,35 @@ def submitAssignmentStudent(request, assignment_id):
     tempAssignment = studentAssignments()
     realAssignment = get_object_or_404(AssignmentTeacherSide, pk=assignment_id) # = assignment =
 
-    if request.method == 'POST':
-        tempAssignment.assignment = realAssignment
-        tempAssignment.points = -1
-        tempAssignment.document = request.FILES['myfile']
-        tempAssignment.studentUser = request.user
-        tempAssignment.save()
-    
+    try:
+        if request.method == 'POST':
+            tempAssignment.assignment = realAssignment
+            tempAssignment.points = -1
+            tempAssignment.document = request.FILES['myfile']
+            tempAssignment.studentUser = request.user
+            tempAssignment.save()
+            messages.info(request, 'Assignment Submitted!')
+    except:
+        messages.info(request, 'Assignment is not Submitted! Please Try again.')
+        pass
+
     return redirect('home')
 
 
 def showGrades(request):
-    if request.method == 'POST':
-        user = User.objects.get(username=request.POST['studentID'])
-        assignments = studentAssignments.objects
-        validAssignments = []
+    validAssignments = []
+    user = ""
+    
+    try:
+        if request.method == 'POST':
+            user = User.objects.get(username=request.POST['studentID'])
+            assignments = studentAssignments.objects
 
-        for assignment in assignments.all():
-            if assignment.studentUser == user:
-                validAssignments.append(assignment)
+            for assignment in assignments.all():
+                if assignment.studentUser == user:
+                    validAssignments.append(assignment)
+    except:
+        pass
 
     return render(request, 'assignments/gradeStudent.html', {"validAssignments": validAssignments, "studentInfo": user})
     
@@ -90,9 +104,11 @@ def gradeStudent(request):
             
             gradeThisAssignment.points = tempGrade
             gradeThisAssignment.save()
-        
+            messages.info(request, 'Assignment Graded!')
+
             return redirect('home')
-    except studentAssignments .DoesNotExist:
+    except studentAssignments.DoesNotExist:
+        messages.info(request, 'Assignment is not ready to be graded!')
         return redirect('home')
 
 
